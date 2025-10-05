0a312f639696d1acc1ea01bdb835c84c87007c2a
Dave Denholm <git@divenal.mooo.com>
2025-10-05 19:34:15 +0100

    add notifier to watch-sensors
    Add the notion of a notifier object which contains the
    notification state and interval. Add a Notifier class, but
    still allow a Sensor class to be used as a notifier - ie anything
    with notified and an interval fields.
    
    Then notify() takes the notifier object and updates it.

--- watch-sensors.py
+++ watch-sensors.py
@@ -10,14 +10,56 @@ import time
 
 from sensors import Sensors
 
+# A notifier contains a notified field and an interval field.
+# former records when it last notified, and interval is how often it should notify.
+# Notification is then throttled to only that interval
+
+# This class is just a generic container, but any object will do. For Sensors, the
+# sensor class itself is used.
+# Arbitary values can be attached to it
+
+class Notifier:
+    notified = 0
+    interval = 8*60*60
+    def __init__(self, interval):
+        self.interval = interval
+
+# Because any object can be a notifier, this isn't a member function
+def notify(notifier, now, msg, priority="4"):
+    """Conditionally send a notification, updating notification time."""
+
+    if notifier is not None:
+        if notifier.notified == 0:
+            pass  # not previously notified
+        elif now < notifier.notified + notifier.interval:
+            return # notified recently
+        notifier.notified = now
+        
+    # This originally used givenergy notifications, but they're forbidding it now ?
+    # Ah - no, I'd forgotten to update the API key !
+    print(msg)
+    requests.post("https://ntfy.sh/divenal14_sensors",
+                  data=msg.encode(encoding='utf-8'),
+                  headers={ "Tags": "warning", "Priority": priority })
+
+def pretty(age):
+    """Given an age in seconds, return a helpful string."""
+    if age > 7200:
+        return f"{age / 3600:.2f} hours"
+    elif age > 300:
+        return f"{age / 60:.2f} minutes"
+    else:
+        return "f{age:d} seconds"
+
+# The sensors we watch
 watchlist = (Sensors.Zappi, Sensors.Daikin, Sensors.GivEnergy, Sensors.IOG, Sensors.GreenerDays)
 
-# for each interesting sensor, stash a expected max update interval (seconds)
-# and set a notified time of 0. We only want to notify once, until
-# it starts working again, so the convention is that if last timestamp
-# is newer than notified time, it must have woken up then died again.
-# But while notified time is newer than instance timestamp, no change
-# since we last notified.
+for s in watchlist:
+    s.notified = 0
+    s.interval = 8*60*60
+
+# Give each sensor class (the notifer) an update interval within which
+# they are expected to have updated.
 
 # for some reason, zappi goes offline when we turn off maple's ethernet
 # takes about 15 mins to recover ?
@@ -27,105 +69,81 @@ Sensors.GivEnergy.update = 600
 Sensors.IOG.update = 4000
 Sensors.GreenerDays.update = 4*60*60*24
 
-# a few other things we stash
-Sensors.Zappi.oldhp = 0
-Sensors.Zappi.oldstatus = 0
-Sensors.Zappi.oldlock = 23
+# A notifier for inverter tripping (non-zero eps)
+eps = Notifier(30*60)
 
-Sensors.GivEnergy.eps = 0
+# zappi fault status
+zstatus = Notifier(10*60)
+zstatus.old = 0
 
-for s in watchlist:
-    s.notified = 0
+# zappi lock
+zlock = Notifier(10*60)
+zlock.old = 23
 
+# hp power - not really a notifier, just somewhere to stash old value
+hp = Notifier(10*60*60)
+hp.old = 0
 
-def notify(msg, priority="4"):
-    # This originally used givenergy notifications, but they're forbidding it now ?
-    # Ah - no, I'd forgotten to update the API key !
-    print(msg)
-    requests.post("https://ntfy.sh/divenal14_sensors",
-                  data=msg.encode(encoding='utf-8'),
-                  headers={ "Tags": "warning", "Priority": priority })
-
-def notify_sensor(sn, age):
-
-    if age == 0:
-        notify(f"sensor {sn} is back online", "3")
-        return
-
-    # turn age in seconds into something more friendly
-    if age > 7200:
-        when = f"{age / 3600:.2f} hours"
-    elif age > 300:
-        when = f"{age / 60:.2f} minutes"
-    else:
-        when = "f{age:d} seconds"
-
-    notify(f"sensor {sn} has not been updated for {when}")
+# filesystem
+fs = Notifier(4*60*60)
 
 def check_sensor(s, now):
     """If sensor of given class is stale, send a notification"""
     sc = s.__class__
-    if s.timestamp + sc.update < now:
-        # sensor is stale
-        if sc.notified < s.timestamp or sc.notified + 8*3600 < now:
-            # either have not notified, or notified 8 hours ago
-            notify_sensor(sc.__name__, now - s.timestamp)
-            sc.notified = now
-    elif sc.notified != 0:
-        # sensor has recovered since notification was sent
-        notify_sensor(sc.__name__, 0)
+    if now > s.timestamp + sc.update:
+        age = pretty(now - s.timestamp)
+        notify(sc, now, f"{sc.__name__} has not updated for {age}")
+    elif (sc.notified):
+        notify(None, now, f"{sc.__name__} back online", "3")
         sc.notified = 0
 
 def check_zappi(sensors, now):
     zappi = sensors.load(Sensors.Zappi)
-    if zappi.status != zappi.oldstatus:
+    if zappi.status != zstatus.old:
         if zappi.status < 0:
-            notify("zappi has faulted ?")
-        Sensors.Zappi.oldstatus = zappi.status
-    if zappi.lock != zappi.oldlock:
+            notify(zstatus, now, "zappi has faulted ?")
+        else:
+            zstatus.notifed = 0
+        zstatus.old = zappi.status
+
+    if zappi.lock != zlock.old:
         # lock is usually 23 when locked, but sometimes 31 ?   22 seems to be unlocked.
         if zappi.lock != 23 and zappi.lock != 31:
-            notify("zappi not locked ?")
-        Sensors.Zappi.oldlock = zappi.lock
+            notify(zlock, now, "zappi not locked ?")
+        elif zlock.notified:
+            notify(None, now, "zappi locked", "3")
+            zlock.notified = 0
 
+        # This was really just for testing
+        # Look to see if daikin has turned on/off recently
+        return
+        if hp.old > 500 and zappi.hp < 500:
+            notify("hp has turned off ?")
+        elif hp.old < 500 and zappi.hp > 500:
+            notify("hp has turned on ?")
+        hp.old = zappi.hp
+        
 def check_ge(sensors, now):
     ge = sensors.load(Sensors.GivEnergy)
     if ge.eps > 0:
-        if Sensors.GivEnergy.eps + 3600 < now:
-            notify("givenergy outage ?")
-            Sensors.GivEnergy.eps = now
-    else:
-        Sensors.GivEnergy.eps = 0
+        notify(eps, now, "givenergy outage ?")
+    elif (eps.notified):
+        notify(None, now, "givenergy back");
+        eps.notified = 0
 
-def check_hp(sensors, now):
-    """Look to see if hp has turned on/off in the last interval."""
-    # previous hp power is stashed in the Sensors.Zappi (class) object.
-    # Assume a min power of 500W when HP is on.
-    zappi = sensors.load(Sensors.Zappi)
-    # print("zappi before/after: ", Sensors.Zappi.oldhp, zappi.hp)
-    if Sensors.Zappi.oldhp > 500 and zappi.hp < 500:
-        notify("hp has turned off ?")
-    elif Sensors.Zappi.oldhp < 500 and zappi.hp > 500:
-        notify("hp has turned on ?")
-    Sensors.Zappi.oldhp = zappi.hp
        
-def check_fs(sensors, now):
+def check_maple(now):
     svfs = os.statvfs("/extra/hts")
     if svfs.f_bavail * svfs.f_bsize < 4*1024*1024*1024:
-        # only 4Gb free
-        if sensors.fs_notified + 8*3600 < now:
-            # last notification was more than 8 hours ago
-            notify("less than 4Gb space available")
-            sensors.fs_notified = now
+        notify(fs, now, "less than 4Gb space available")
     else:
-        sensors.fs_notified = 0
+        fs.notified = 0
+
+    # TODO: add a check for loadavg, which goes a bit
+    # bonkers from time to time
 
 def main():
     sensors = Sensors()
-
-    # hang timestamp of disk notification off that
-    # (just because python makes a fuss about globals)
-    sensors.fs_notified = 0
 
     while True:
 
@@ -137,13 +155,10 @@ def main():
             s = sensors.load(sc)
             check_sensor(s, now)
 
-        # check_hp(sensors, now)
         check_zappi(sensors, now)
         check_ge(sensors, now)
-        check_fs(sensors, now)
+        check_maple(now)
 
-        # TODO: add a check for loadavg, which goes a bit
-        # bonkers from time to time
         time.sleep(30)
 
 if __name__ == "__main__":
